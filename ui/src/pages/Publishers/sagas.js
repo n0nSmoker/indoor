0435456573c0faee97847e28f419064b7beab691
Philipp.Ilin <filin@beeline.ru>
2020-02-19 12:32:24 +0300

    publishers search and refactoring

--- ui/src/pages/Publishers/sagas.js
+++ ui/src/pages/Publishers/sagas.js
@@ -1,41 +1,62 @@
 import { all, call, put, select, takeLatest, takeEvery } from 'redux-saga/effects';
 
-import requests from '../../lib/requests.js';
+import { showNetworkError, showSuccess } from 'components/Notifications/actions';
+import requests from 'lib/requests';
 
+import { fetchPublishers } from './actions';
 import {
   PUBLISHERS_ACTION_FETCH_PUBLISHERS,
   PUBLISHERS_ACTION_RECEIVE_PUBLISHERS,
-  PUBLISHERS_ACTION_ADD_PUBLISHER,
-  PUBLISHERS_ACTION_UPDATE_PUBLISHER,
+  PUBLISHERS_ACTION_MUTATE_PUBLISHER,
   PUBLISHERS_ACTION_DELETE_PUBLISHER,
-} from './consts.js';
+} from './consts';
+
 
 function* fetchPublishersWorker() {
-  const filters = yield select(state => state.publishersReducer.filters);
-  const response = yield call(requests.get, '/publishers/', filters);
-  yield put({type: PUBLISHERS_ACTION_RECEIVE_PUBLISHERS, publishers: response.data})
+  try {
+    const filters = yield select(
+      ({publishersReducer: { filters }, headerReducer: { search }}) => ({
+        ...filters,
+        ...(search.value ? {query: search.value} : {}),
+      })
+    );
+    const response = yield call(requests.get, '/publishers/', filters);
+    yield put({type: PUBLISHERS_ACTION_RECEIVE_PUBLISHERS, publishers: response.data});
+  } catch (e) {
+    console.log(e);
+    yield put(showNetworkError(e));
+  }
 }
 
-function* addPublisherWorker(action) {
-  yield call(requests.post, '/publishers/', action.formData);
-  if (action.callback) action.callback();
+function* mutatePublishersWorker({ payload: { id, ...formData }, callback }) {
+  try {
+    if(id) {
+      yield call(requests.put, `/publishers/${id}/`, formData);
+    } else {
+      yield call(requests.post, '/publishers/', formData);
+    }
+    yield put(fetchPublishers());
+    yield put(showSuccess(`Рекламодатель успешно ${id ? 'обновлен' : 'добавлен'}`));
+    if (callback) callback();
+  } catch (e) {
+    yield put(showNetworkError(e))
+  }
 }
 
-function* updatePublisherWorker(action) {
-  yield call(requests.put, `/publishers/${action.publisherId}/`, action.formData);
-  if (action.callback) action.callback();
-}
-
-function* deletePublisherWorker(action) {
-  yield call(requests.delete, `/publishers/${action.publisherId}/`);
-  if (action.callback) action.callback();
+function* deletePublishersWorker(action) {
+  try {
+    yield call(requests.delete, `/publishers/${action.payload}/`);
+    yield put(fetchPublishers());
+    yield put(showSuccess('Рекламодатель успешно удален'));
+  } catch (e) {
+    yield put(showNetworkError(e));
+  }
 }
 
 export default function* () {
   yield all([
     takeLatest(PUBLISHERS_ACTION_FETCH_PUBLISHERS, fetchPublishersWorker),
-    takeLatest(PUBLISHERS_ACTION_ADD_PUBLISHER, addPublisherWorker),
-    takeLatest(PUBLISHERS_ACTION_UPDATE_PUBLISHER, updatePublisherWorker),
-    takeEvery(PUBLISHERS_ACTION_DELETE_PUBLISHER, deletePublisherWorker),
+    takeLatest(PUBLISHERS_ACTION_MUTATE_PUBLISHER, mutatePublishersWorker),
+    takeEvery(PUBLISHERS_ACTION_DELETE_PUBLISHER, deletePublishersWorker),
   ]);
 }
